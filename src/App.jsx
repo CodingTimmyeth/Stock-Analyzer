499d0589b8b5ad3fffc7aa2e58ea63fb6f581541
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-07-27 15:45:26 -0700

    header finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from "react";
 import "./App.css";
-import { FaSearch } from "react-icons/fa";
+import { GrSearch } from "react-icons/gr";
+import { FcCandleSticks } from "react-icons/fc";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-red-500">Hello World</h1>
-      <FaSearch />
+      <header className="flex flex-col items-start gap-5">
+        <div className="bg-secondaryBG p-2 rounded-full">
+          <FcCandleSticks className="text-3xl" />
+        </div>
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-2">
+            <label className="text-textWhite">Search for Stock...</label>
+            <input
+              className={`px-5 py-3 border-none rounded-2xl`}
+              type="text"
+              placeholder="APPL"
+            />
+          </div>
+          <div className="flex justify-center items-end text-textWhite">
+            <button className="bg-primaryAccent py-3 px-5 rounded-2xl">
+              Analyze
+            </button>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBG: '#171C27',
+        secondaryBG: '#212936',
+        textWhite: '#F7F7F7',
+        primaryAccent: '#00C806',
+        secondaryAccent: '#FF4848',
+        neutral: '#A5A5A5',
+        positive: '#39C33E',
+        negative: '#F34336',
+        neutralDark: '#3E495D'
+      },
+    },
   },
   plugins: [],
 }
